ff140af46e63da75ecd4003b7606abc0d763fd7a
Emartinez2002 <e.martineza126@gmail.com>
2022-11-05 15:48:27 -0400

    ejm

--- Ejemplos.py
+++ Ejemplos.py
@@ -1,1 +1,8 @@
-print("ejemplos")
+def atras(segundos):
+    segundos = input("ingrese la cantidad: ")
+    segundos -= 1
+    if segundos > 0:
+        print(segundos)
+
+    else:
+        print("termino")
