3faaaf7d3acea48555850734216b02ee8831dc62
Emartinez2002 <e.martineza126@gmail.com>
2022-11-04 20:49:49 -0400

    encriptamiento y desencriptamiento
    se creo la encriptación y la desencriptación de las contraseñas

--- Ejemplos.py
+++ Ejemplos.py
@@ -1,5 +1,27 @@
-from collections import deque
+from cryptography.fernet import Fernet
+# antes de utilizar es necesario instalar lo siguiente pip install cryptography
 
-colas = deque(['juan','pedro','dido','pedrito'])
-colas.pop()
-print(colas)
+
+
+while True:
+    print('''
+    1.encriptar 
+    2.desencriptar 
+    3.salir
+    ''')
+    opcion = input("ingrese la opcion: ")
+    if opcion == "1":
+        texto = input("ingrese contraseña: ")
+        key = Fernet.generate_key()
+        clave_cifrada = Fernet(key)
+        clava_encriptada = clave_cifrada.encrypt(str.encode(texto))
+        print(clava_encriptada)
+    elif opcion == "2":
+        texto = input("ingrese la contraseña encriptada: ")
+        desencriptado = clave_cifrada.decrypt(clava_encriptada)
+        texto_desencriptado = desencriptado.decode()
+        print(texto_desencriptado)
+
+    else:
+        print("apagado")
+        break
--- funciones/comprobar.py
+++ funciones/comprobar.py
@@ -1,22 +1,35 @@
+from cryptography.fernet import Fernet
+
 def comprobar():
-    print('''
-    1.Comprobar Contraseñas
-    2.Encriptar Contraseñas
-    3.Desencriptar Contraseñas
-    4.Salir
-    ''')
-    opcion = int(input("ingrese opcion: "))
-    if opcion == 1:
-        print("Comprobacion de contraseña ")
-        contraseña = len(input("Ingrese Contraseña: "))
-        #como primer punto para que la contraseña robusta debe contar con almenos 12 caracteres
-        if contraseña < 8:
-                print("su contraseña es vunerable es necesario cambiarla")
+    while True:
+        print('''
+        1.Comprobar Contraseñas
+        2.Encriptar Contraseñas
+        3.Desencriptar Contraseñas
+        4.Salir
+        ''')
+        opcion = int(input("ingrese opcion: "))
+        if opcion == 1:
+            print("Comprobacion de contraseña ")
+            contraseña = len(input("Ingrese Contraseña: "))
+            #como primer punto para que la contraseña robusta debe contar con almenos 12 caracteres
+            if contraseña < 8:
+                    print("su contraseña es vunerable es necesario cambiarla")
+            else:
+                print("contraseña segura")
+        elif opcion == 2:
+            print("Encriptar Contraseña")
+            contraseña = input("Ingrese Contraseña: ")
+            key = Fernet.generate_key()
+            clave_cifrada = Fernet(key)
+            clave_encriptada = clave_cifrada.encrypt(str.encode(contraseña))
+            print(clave_encriptada)
+        elif opcion == 3:
+            print("Desencriptar Contraseña")
+            contraseña = input("Ingrese Contraseña: ")
+            desencriptado = clave_cifrada.decrypt(clave_encriptada)
+            contraseña_desencriptada =  desencriptado.decode()
+            print(contraseña_desencriptada)
         else:
-            print("contraseña segura")
-    if opcion == 2:
-        print("Encriptar Contraseña")
-        contraseña = int(input("Ingrese Contraseña: "))
-    else:
-        print("Salio de comprobador de contraseñas")
-        
+            print("Salio de comprobador de contraseñas")
+            
