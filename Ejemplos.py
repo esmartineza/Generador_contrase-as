17a3063304927eb8f049fac716db561f02c2f0ba
Emartinez2002 <e.martineza126@gmail.com>
2022-11-12 15:34:47 -0400

    ejemplos

--- Ejemplos.py
+++ Ejemplos.py
@@ -1,3 +1,9 @@
-class auto:
-    pass
-print(auto)
+class Auto:
+    Rojo = False
+    def __init__(self):
+        print("se creo un auto")
+    def Fabricar(self):
+        Rojo = True
+a = Auto()
+
+print(a.Fabricar)
